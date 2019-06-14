91be6fa95ec19ae9e88410ba6bec42a082d49bdc
Ignacio Di Leva <ignaciodileva@gmail.com>
2019-06-14 14:57:36 -0300

    Removed unnecessary files

--- chmod400.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-CD /D "%~dp0"
-powershell.exe -ExecutionPolicy Unrestricted .\chmod400.ps1
--- chmod400.ps1
+++
@@ -1,7 +0,0 @@
-$path = '.\key-pair-test.pem'
-$acl = Get-Acl $path
-$acl.SetAccessRuleProtection($true,$false)
-$acl.Access | ForEach-Object { $acl.RemoveAccessRule($_) }
-$ace = New-Object System.Security.AccessControl.FileSystemAccessRule -ArgumentList $acl.Owner, 'Read', 'Allow'
-$acl.AddAccessRule($ace)
-Set-Acl -Path $path -AclObject $acl
